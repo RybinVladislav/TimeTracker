878224f3681b1a6b3c6dcb14ed5d1079ca46bf9e
RybinVladislav <rybin@me.com>
2016-03-15 23:17:02 +0000

    Json validation extensions

--- app/formatters/json/TimeEntryFormats.scala
+++ app/formatters/json/TimeEntryFormats.scala
@@ -1,6 +1,7 @@
 package formatters.json
 
 import models.{EntryStatus, TimeEntry, User}
+import play.api.data.validation.ValidationError
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
@@ -8,6 +9,11 @@ object TimeEntryFormats {
   val restFormat = {
     implicit val enumFormat = EntryStatus.statusEnumFormat
     implicit val userFormat = formatters.json.UserFormats.restFormat
+
+    val onlyValidQuantity: Reads[Long] =
+      Reads.LongReads.filter(ValidationError("Invalid hours given!"))(hours => {
+        hours < 24
+      })
 
     val entryWrites = new Writes[TimeEntry] {
       def writes(entry: TimeEntry) = Json.obj(
@@ -24,7 +30,7 @@ object TimeEntryFormats {
       (JsPath \ "id").read[Long] and
         (JsPath \ "user").read[User] and
         (JsPath \ "date").read[String] and
-        (JsPath \ "quantity").read[Long] and
+        (JsPath \ "quantity").read[Long](onlyValidQuantity) and
         (JsPath \ "description").read[String] and
         (JsPath \ "status").read[EntryStatus.Value]
       )(TimeEntry.apply _)
